07ed2317987a520af3348c358db37cd0182577dd
LongJumpProduction <118313406+LongJumpProduction@users.noreply.github.com>
2024-10-18 12:01:38 +0200

    Small edits

--- assets/sql/sql-queries/etterspoersel-over-tid.sql
+++ assets/sql/sql-queries/etterspoersel-over-tid.sql
@@ -6,12 +6,12 @@ WITH months AS (
     )), 'yyyy-mm') AS order_month
 )
 SELECT
-    'Geitost' AS prod_name, -- Manually specify the product name to avoid NULLs
-    m.order_month,
-    COUNT(CASE WHEN p.product_name = 'Geitost' THEN od.order_id END) AS number_of_sales -- Count only for Geitost
-FROM months m
-LEFT JOIN orders o ON to_char(date_trunc('month', o.order_date), 'yyyy-mm') = m.order_month
-LEFT JOIN order_details od ON od.order_id = o.order_id
-LEFT JOIN products p ON od.product_id = p.product_id
-GROUP BY m.order_month
-ORDER BY m.order_month;
+    'Geitost' AS prod_name,
+    months.order_month,
+    COUNT(CASE WHEN products.product_name = 'Geitost' THEN order_details.order_id END) AS number_of_sales
+FROM months
+LEFT JOIN orders ON to_char(date_trunc('month', orders.order_date), 'yyyy-mm') = months.order_month
+LEFT JOIN order_details ON order_details.order_id = orders.order_id
+LEFT JOIN products ON order_details.product_id = products.product_id
+GROUP BY months.order_month
+ORDER BY months.order_month;
