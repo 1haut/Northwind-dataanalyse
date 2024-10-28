556ea297fe6710999ae5027fe696d26e27cab24c
LongJumpProduction <118313406+LongJumpProduction@users.noreply.github.com>
2024-10-28 14:57:10 +0100

    TBC

--- delete.js
+++ delete.js
@@ -15,11 +15,12 @@ const db = new pg.Client({
 db.connect();
 
 db.query("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'")
+// db.query("INSERT INTO employees(employee_id, first_name, last_name, notes) VALUES(10, 'Previous', 'Employee', '')")
 
 function resArray(obj) {
     let arr = [];
     obj.forEach(element => {
-        arr.push(element.table_name || element.column_name)
+        arr.push(element.table_name || element.column_name || element.constraint_name)
     });
     return arr
 }
@@ -30,7 +31,6 @@ async function sgo(query) {
     let arrColumns = [];
     let currentTable
 
-    // query = "SELECT constraint_name FROM information_schema.constraint_table_usage WHERE table_name = 'suppliers'"
     // db.query(query, (err, res) => {
     //     if (err) {
     //         console.log(err.stack)
@@ -57,23 +57,43 @@ async function sgo(query) {
 
     const results2 = await db.query("SELECT column_name FROM information_schema.columns WHERE table_schema = 'public' AND table_name = $1", [currentTable])
     arrColumns = resArray(results2.rows)
+    console.log(arrColumns)
 
-    const key = arrColumns[0];
-    console.log(key)
+    const keyColumn = arrColumns[0];
+    console.log(keyColumn)
 
+    const constraints = await db.query("SELECT constraint_name FROM information_schema.constraint_table_usage WHERE table_name = $1", [currentTable])
+    console.log(resArray(constraints.rows))
 
+    const newString = `SELECT ${keyColumn} FROM ${currentTable} ` + query.slice(query.indexOf("WHERE"))
+    console.log(newString)
+
+    const queryId = await db.query(`SELECT ${keyColumn} FROM ${currentTable} ` + query.slice(query.indexOf("WHERE")))
+    const rowId = queryId.rows[0][keyColumn]
+    console.log(rowId)
+
+    let arrTables2 = arrTables.filter(table => table !== currentTable);
+    console.log(arrTables2)
+
+    // try {
+    //     arrTables2.forEach(table => {
+    //         db.query(`UPDATE ${table} SET ${keyColumn} = NULL WHERE ${keyColumn} = ${rowId}`)
+    //     })
+    // } catch (err) {
+    //     console.log(err)
+    // }
 }
 
 const text = "DELETE FROM employees WHERE last_name = 'Peacock'"
 
-// sgo(text)
-const newText = "DELETE FROM employees WHERE last_name = 'Peacock'"
+sgo(text)
+// const newText = "DELETE FROM employees WHERE last_name = 'Peacock'"
 
-console.log(
-    newText.indexOf("WHERE"))
+// console.log(
+//     newText.indexOf("WHERE"))
 
-console.log(newText.slice(22))
+// console.log(newText.slice(22))
 
-console.log(newText.slice(22).split(" "))
+// console.log(newText.slice(22).split(" "))
 
-console.log(newText.slice(22).split(" ")[1], newText.slice(22).split(" ")[3])
+// console.log(newText.slice(22).split(" ")[1], newText.slice(22).split(" ")[3])
